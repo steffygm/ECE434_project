65beac25f0fbd4e407b5bc48ca0100675ecd2285
Russmania101 <johnsor2@rose-hulman.edu>
2018-10-27 18:26:33 +0000

    added in code for test with both 7 segs

--- examples/ex_7segment_clock.py
+++ examples/ex_7segment_clock.py
@@ -8,10 +8,12 @@ from Adafruit_LED_Backpack import SevenSegment
 # ===========================================================================
 # Clock Example
 # ===========================================================================
-segment = SevenSegment.SevenSegment(address=0x70)
+segment1 = SevenSegment.SevenSegment(address=0x70)
+segment2 = SevenSegment.SevenSegment(address=0x71)
 
-# Initialize the display. Must be called once before using the display.
-segment.begin()
+# Initialize the displays. Must be called once before using the displays.
+segment1.begin()
+segment2.begin()
 
 print "Press CTRL+Z to exit"
 
@@ -22,19 +24,29 @@ while(True):
   minute = now.minute
   second = now.second
 
-  segment.clear()
+  segment1.clear()
+  segment2.clear()
+  
   # Set hours
-  segment.set_digit(0, int(hour / 10))     # Tens
-  segment.set_digit(1, hour % 10)          # Ones
+  segment1.set_digit(0, int(hour / 10))     # Tens
+  segment1.set_digit(1, hour % 10)          # Ones
+  segment2.set_digit(0, int(hour / 10))     # Tens
+  segment2.set_digit(1, hour % 10)          # Ones
+  
   # Set minutes
-  segment.set_digit(2, int(minute / 10))   # Tens
-  segment.set_digit(3, minute % 10)        # Ones
+  segment1.set_digit(2, int(minute / 10))   # Tens
+  segment1.set_digit(3, minute % 10)        # Ones
+  segment2.set_digit(2, int(minute / 10))   # Tens
+  segment2.set_digit(3, minute % 10)        # Ones
+
   # Toggle colon
-  segment.set_colon(second % 2)              # Toggle colon at 1Hz
+  segment1.set_colon(second % 2)              # Toggle colon at 1Hz
+  segment2.set_colon(second % 2)              # Toggle colon at 1Hz
 
   # Write the display buffer to the hardware.  This must be called to
   # update the actual display LEDs.
-  segment.write_display()
+  segment1.write_display()
+  segment2.write_display()
 
   # Wait a quarter second (less than 1 second to prevent colon blinking getting$
   time.sleep(0.25)

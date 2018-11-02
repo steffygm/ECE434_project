75fdab2d89e843717c9b8a9149892ecb234514ea
Russell Johnson <johnsor2@rose-hulman.edu>
2018-11-02 18:12:57 -0400

    update code to trigger all ir sensors to update score

--- examples/paper_football.py
+++ examples/paper_football.py
@@ -21,74 +21,52 @@ segment1.begin()
 segment2.begin()
 
 # setup variables for detecting if an ir beam has been broken
+any_broken = 0
 broken1 = 0
 broken2 = 0
 broken3 = 0
 broken4 = 0
 broken5 = 0
 
+# variables to track players scores
+home_score = 0
+away_score = 0
+
 print "Press CTRL+Z to exit"
 
+#segment.set_digit(0, 1) # set tens digits of left side of display to 1
+#segment.set_digit(1, 1) # set ones digits of left side of display to 1
+#segment.set_digit(2, 1) # set tens digits of right side of display to 1
+#segment.set_digit(3, 1) # set ones digits of right side of display to 1
+#segment.set_colon(freq) # set colon at freq
+
+# set both scores to 0
+segment1.clear()
+segment2.clear()
+
+# set each score to 10
+segment1.set_digit(3, 0)        # Ones
+segment2.set_digit(3, 0)        # Ones
+
 while (True):
-    segment1.clear()
-    segment2.clear()
-    
-    # Set hours
-    #segment1.set_digit(0, int(hour / 10))     # Tens
-    #segment1.set_digit(1, hour % 10)          # Ones
-    #segment2.set_digit(0, int(hour / 10))     # Tens
-    #segment2.set_digit(1, hour % 10)          # Ones
-    
-    # Set minutes
-    segment1.set_digit(2, 1)   # Tens
-    segment1.set_digit(3, 0)        # Ones
-    segment2.set_digit(2, 1)   # Tens
-    segment2.set_digit(3, 0)        # Ones
+    if not GPIO.input("P9_11") or not GPIO.input("P9_15") or not GPIO.input("P9_13") or not GPIO.input("P9_14") or not GPIO.input("P9_16"):
+        if not any_broken:
+            any_broken = 1
+            print("Borken")
 
-    # Toggle colon
-    #segment1.set_colon(second % 2)              # Toggle colon at 1Hz
-    #segment2.set_colon(second % 2)              # Toggle colon at 1Hz
+            home_score += 6
+            away_score += 6
 
-    # Write the display buffer to the hardware.  This must be called to
-    # update the actual display LEDs.
-    segment1.write_display()
-    segment2.write_display()
+            segment1.clear()
+            segment2.clear()
 
-    # Wait a quarter second (less than 1 second to prevent colon blinking getting$
-    #time.sleep(0.25)
+            segment1.set_digit(2, int(home_score / 10))
+            segment1.set_digit(3, home_score % 10)
+            segment2.set_digit(2, int(away_score / 10))
+            segment2.set_digit(3, away_score % 10) 
 
-    # ir sensors
-    if not GPIO.input("P9_11"):
-            if not broken1:
-                    broken1 = 1
-                    print("Broken1")
-    else:
-            broken1 = 0
-    
-    if not GPIO.input("P9_15"):
-            if not broken2:
-                    broken2 = 1
-                    print("Broken2")
-    else:
-            broken2 = 0
+            segment1.write_display()
+            segment2.write_display() 
 
-    if not GPIO.input("P9_13"):
-            if not broken3:
-                    broken3 = 1
-                    print("Broken3")
-    else:
-            broken3 = 0
-    
-    if not GPIO.input("P9_14"):
-            if not broken4:
-                    broken4 = 1
-                    print("Broken4")
-    else:
-            broken4 = 0
-
-    if not GPIO.input("P9_16"):
-            if not broken5:
-                    broken5 = 1
-                    print("Broken5")
-    else:
-            broken5 = 0
+    else
+        any_broken = 0

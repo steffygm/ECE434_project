620acfecaa94be9c252ae6b2ff0eb0717f1bdcf6
Russell Johnson <johnsor2@rose-hulman.edu>
2018-11-03 19:20:10 -0400

    got game working with ir sensors, 7 segments, and button

--- examples/paper_football.py
+++ examples/paper_football.py
@@ -44,9 +44,13 @@ last = 0
 segment1.clear()
 segment2.clear()
 
-# set each score to 00
+# set each score to 0000
+segment1.set_digit(0, 0)        # Tens
+segment1.set_digit(1, 0)        # Ones
 segment1.set_digit(2, 0)        # Tens
 segment1.set_digit(3, 0)        # Ones
+segment2.set_digit(0, 0)        # Tens
+segment2.set_digit(1, 0)        # Ones
 segment2.set_digit(2, 0)        # Tens
 segment2.set_digit(3, 0)        # Ones
 
@@ -56,37 +60,65 @@ segment2.write_display()
 
 print "Press CTRL+Z to exit"
 
-while (True):
-    if GPIO.input("P9_26") and pressed == 0:
-        pressed = 1
-
+def ir_broken(channel):
+    global last, current, home_score, away_score, segment1, segment2
+    if last != current :
         last = current
 
-        if current:
-            current = 0
-            print "Away Team's Turn"
+        if current == 1:
+            print "Home Team Scored"
+            home_score += 10
+            score_str = str(home_score)
+            if len(score_str) < 3:
+                score_str = "00" + score_str
+            elif len(score_str) < 4:
+                score_str = "0" + score_str
+
+            segment2.clear()
+            segment2.set_digit(0, int(score_str[0]))
+            segment2.set_digit(1, int(score_str[1]))
+            segment2.set_digit(2, int(score_str[2]))
+            segment2.set_digit(3, int(score_str[3])) 
+            segment2.write_display()
+
         else:
-            current = 1
-            print "Home Team's Turn"
-    elif not GPIO.input("P9_26"):
-        pressed = 0
+            print "Away Team Scored"
+            away_score += 10
+            score_str = str(away_score)
+            if len(score_str) < 3:
+                score_str = "00" + score_str
+            elif len(score_str) < 4:
+                score_str = "0" + score_str
 
-    if not GPIO.input("P9_11") or not GPIO.input("P9_15") or not GPIO.input("P9_13") or not GPIO.input("P9_14") or not GPIO.input("P9_16"):
-        if last != current :
-            last = current
+            segment1.clear()
+            segment1.set_digit(0, int(score_str[0]))
+            segment1.set_digit(1, int(score_str[1]))
+            segment1.set_digit(2, int(score_str[2]))
+            segment1.set_digit(3, int(score_str[3]))
+            segment1.write_display()
 
-            if current == 1:
-                print "Home Team Scored"
-                home_score += 6
-                segment2.clear()
-                segment2.set_digit(2, int(home_score / 10))
-                segment2.set_digit(3, home_score % 10) 
-                segment2.write_display()
+def btn_press(channel):
+    global last, current
+    last = current
 
-            else:
-                print "Away Team Scored"
-                away_score += 6
-                segment1.clear()
-                segment1.set_digit(2, int(away_score / 10))
-                segment1.set_digit(3, away_score % 10)
-                segment1.write_display()
+    if current:
+        current = 0
+        print "Away Team's Turn"
+    else:
+        current = 1
+        print "Home Team's Turn"
+    
+GPIO.add_event_detect("P9_11", GPIO.FALLING, callback=ir_broken)
+GPIO.add_event_detect("P9_13", GPIO.FALLING, callback=ir_broken)
+GPIO.add_event_detect("P9_14", GPIO.FALLING, callback=ir_broken)
+GPIO.add_event_detect("P9_15", GPIO.FALLING, callback=ir_broken)
+GPIO.add_event_detect("P9_16", GPIO.FALLING, callback=ir_broken)
+GPIO.add_event_detect("P9_26", GPIO.RISING, callback=btn_press, bouncetime=200)
+
+
+def main():        
+    # empty
+    print "Home Team's Turn"
+    while(True):
+        test = 1
+main()
